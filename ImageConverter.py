c6cc54f07ecf68b5865f345a1cb5ca07a4ef89b6
Markus Geister <megatueftler94@gmail.com>
2024-01-13 23:41:13 +0100

    new tests for player

---
+++ ImageConverter.py
@@ -0,0 +1,20 @@
+from PIL import Image
+from numpy import asarray
+import binascii
+
+image = Image.open('frame015.bmp')
+data = asarray(image)
+#print(data)
+#print (''.join('{:02x}'.format(x) for x in data))
+hexdata = binascii.hexlify(bytearray(data))
+#print (hexdata)
+hexdata1 = hexdata.decode("utf-8")
+#print(hexdata1)
+hexdata_2 = ', 0x'.join(hexdata1[i:i+6] for i in range(0, len(hexdata1), 6))
+#print (hexdata_2)
+hexdata_3 = "const unsigned int arrayx[]PROGMEM = { 0x" + hexdata_2 +"}"
+print (len(hexdata_3))
+print (hexdata_3)
+
+#Scaling images in future
+#https://opensource.com/life/15/2/resize-images-python
---
+++ mp3towav.py
@@ -0,0 +1,10 @@
+from os import path
+from pydub import AudioSegment
+
+# files
+src = "test_song.mp3"
+dst = "test_song.wav"
+
+# convert wav to mp3
+sound = AudioSegment.from_mp3(src)
+sound.export(dst, format="wav")
